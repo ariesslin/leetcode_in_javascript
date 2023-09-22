78f8ee6ae1a4f90e70b1a9c75cb07e7e8d8a8ea9
Ariess Lin <ariess.lin@gmail.com>
2023-09-22 13:33:35 +0200

    Solve the question 76

---
+++ a0076-minimum-window-substring.js
@@ -0,0 +1,49 @@
+function findMinWindow(source, target) {
+    const MAX_LENGTH = Number.POSITIVE_INFINITY;
+
+    let windowChars = new Map();
+    let requiredChars = new Map();
+    for (let char of target) {
+        requiredChars.set(char, (requiredChars.get(char) || 0) + 1);
+    }
+
+    let validCharCount = 0;
+    let startIndex = 0;
+    let minLength = MAX_LENGTH;
+
+    let left = 0,
+        right = 0;
+
+    for (; right < source.length; right++) {
+        let rightChar = source[right];
+        windowChars.set(rightChar, (windowChars.get(rightChar) || 0) + 1);
+        if (windowChars.get(rightChar) === requiredChars.get(rightChar)) {
+            validCharCount++;
+        }
+
+        while (validCharCount === requiredChars.size) {
+            if (right - left < minLength) {
+                startIndex = left;
+                minLength = right - left;
+            }
+
+            let leftChar = source[left];
+            left++;
+            if (windowChars.get(leftChar) === requiredChars.get(leftChar)) {
+                validCharCount--;
+            }
+            windowChars.set(leftChar, windowChars.get(leftChar) - 1);
+        }
+    }
+
+    return minLength === MAX_LENGTH
+        ? ""
+        : source.substring(startIndex, startIndex + minLength + 1);
+}
+
+describe("76. Minimum Window Substring", () => {
+    test("should output BANC from ADOBECODEBANC, ABC", () => {
+        const result = findMinWindow("ADOBECODEBANC", "ABC");
+        expect(result).toBe("BANC");
+    });
+});
